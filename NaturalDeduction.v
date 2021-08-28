1576c46da3980f0f7d1e8595c0484395c80b01ae
Wags69 <bhargav.kulkarni1@gmail.com>
2021-08-28 17:56:39 +0530

    Natural deduction working formalization

--- NaturalDeduction.v
+++ NaturalDeduction.v
@@ -3,8 +3,8 @@ Load PropLogic.
 Reserved Notation "U |- A" (at level 80).
 Inductive Nd: list Proposition -> Proposition -> Prop :=
 | Assumption U A (H: In A U): U |- A
-| NotE U P (H: U |- ¬ (¬ P)): U |- P
-| NotI U P Q (H1: U |- P) (H2: U |- Q) (H3: U |- ¬ Q): U |- ¬ P
+| NotE U P (H1: U |- ¬ ¬ P) : U |- P
+| NotI U P Q (H1: P::U |- Q) (H2: P::U |- ¬ Q): U |- ¬ P
 | AndE1 U A B (H: U |- (A ∧ B)): U |- A
 | AndE2 U A B (H: U |- (A ∧ B)): U |- B
 | AndI U A B (H1: U |- A) (H2: U |- B): U |- (A ∧ B)
@@ -26,18 +26,84 @@ Proof.
   reflexivity.
 Qed.
 
-Theorem and_introduction: forall p q,
-  [p; q] |- p ∧ q.
+Example example_1': forall p q,
+  [¬ p; q]|- ¬(p ∧ q).
 Proof.
   intros p q.
-  apply AndI.
-  - apply Assumption. unfold In. left. reflexivity.
-  - apply Assumption. unfold In. right. left. reflexivity.
+  apply (NotI _ _ p).
+  - apply (AndE1 _ _ q). apply Assumption. simpl. left. reflexivity.
+  - apply Assumption. simpl. right. left. reflexivity.
 Qed.
 
-Theorem or_distributes_over_and: forall p q r,
-  [(p∨(q∧r))] |- (p∨q)∧(p∨r).
+Example example_2': forall fire smoke rain,
+  [fire → smoke; rain → ¬ smoke] |- rain → ¬ fire.
 Proof.
-  intros p q r.
-  apply AndI.
-Abort.
+  intros fire smoke rain.
+  apply ImpI.
+  apply (NotI _ _ smoke).
+  - apply (ImpE _ fire _).
+    + apply Assumption. simpl. left. reflexivity.
+    + apply Assumption. simpl. right. right. left. reflexivity.
+  - apply (ImpE _ rain _).
+    + apply Assumption. simpl. right. left. reflexivity.
+    + apply Assumption. simpl. right. right. right. left. reflexivity.
+Qed.
+
+Theorem principle_of_explosion: forall p q,
+  [p ; ¬ p] |- q.
+Proof.
+  intros.
+  apply NotE.
+  apply (NotI _ _ p)
+  ;apply Assumption
+  ;simpl.
+  - right. left. reflexivity.
+  - right. right. left. reflexivity.
+Qed.
+
+Theorem law_of_excluded_middle: forall p,
+  [] |- p ∨ ¬ p.
+Proof.
+  intros.
+  apply NotE.
+  apply (NotI _ _ ¬ p).
+  - apply (NotI _ _ (p ∨ ¬ p)).
+    + apply OrI1.
+      apply Assumption.
+      left.
+      reflexivity.
+    + apply Assumption. right. left. reflexivity.
+  - apply (NotI _ _ (p ∨ ¬ p)).
+    + apply OrI2.
+      apply Assumption.
+      left.
+      reflexivity.
+    + apply Assumption. right. left. reflexivity.
+Qed.
+
+Theorem double_not_introduction: forall p,
+  [p] |- ¬ ¬ p.
+Proof.
+  intros p.
+  apply (NotI _ _ p)
+  ; apply Assumption
+  ; simpl.
+  - right. left. reflexivity.
+  - left. reflexivity.
+Qed.
+
+Theorem contrapositve: forall p q,
+  [p → q; ¬ q] |- ¬ p.
+Proof.
+  intros.
+  apply (NotI _ _ q).
+  - apply (ImpE _ p _)
+    ; apply Assumption
+    ; simpl.
+    + left. reflexivity.
+    + right. left. reflexivity.
+  - apply Assumption.
+    simpl.
+    right. right. left.
+    reflexivity.
+Qed.
